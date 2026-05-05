ea3e612dc2984d161f40072d9cd757240e7a4952
Jonathan Torres <jonathan.torresmo@amigo.edu.com>
2026-05-05 16:34:31 -0500

    se crean las consultas en la BD y se añaden los manuales técnico y de  usuario y se actualiza el README.md

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import AppNavigator from './navigation/AppNavigator';
 import AppProvider from './navigation/AppProvider';
 import { AuthProvider } from './navigation/AuthContext';
 import { NavigationContainer } from '@react-navigation/native';
+import sqliteService from './src/services/sqliteService';
 
 export default function App() {
+
+  useEffect(() => {
+    sqliteService.init();
+  }, []);
+
   return (
     <AppProvider>
       <AuthProvider>
